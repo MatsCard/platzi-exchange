87de33e69cc72591aea8fa23f95d81c5b5cf2f01
Cardone, Matias <zero.xm7@gmail.com>
2020-11-15 14:31:28 -0300

    header fix

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 <template>
-  <header class="shadow w-screen">
+  <header class="shadow">
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <div class="flex items-center flex-shrink-0 text-white pr-6">
           <px-icon class="mr-2" />
           <router-link
             :to="{ name: 'home' }"
